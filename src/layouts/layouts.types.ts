9777b74d752b7849e888ec6dfc8b25a867392e5d
MohammedYoussefSoliman <m.youssef.soliman@gmail.com>
2024-02-29 17:58:45 +0200

    added aside types

--- src/layouts/layouts.types.ts
+++ src/layouts/layouts.types.ts
@@ -3,3 +3,21 @@ import React from "react";
 export type LayoutProps = {
   children: React.ReactNode;
 };
+
+export type MenuItem = {
+  type: "item";
+  path: string;
+  label: string;
+  icon?: (active: boolean) => React.ReactNode;
+};
+
+export type SideMenu = {
+  type: "menu";
+  label: string;
+  icon?: React.ReactNode;
+  items: MenuItem[];
+};
+
+export type SideNavProps = {
+  navItems: (SideMenu | MenuItem)[];
+};
